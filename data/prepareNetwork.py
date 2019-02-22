2703eed44094141d44688f9ed38de664ec6c56ae
Jesus Cabrera Alvarez <jescab01@ucm.es>
2019-02-22 19:27:58 +0000

    Dando forma al codigo

--- data/prepareNetwork.py
+++ data/prepareNetwork.py
@@ -6,23 +6,22 @@ Created on Fri Feb 15 10:14:54 2019
 @author: jescab01
 
 This code will process raw data from different sources (go to readme.txt)
-and update a basic banch of nodes with defined topology.
+and define a basic banch of nodes with defined topology, connections 
+and neurotransmitter type.
 
-Include connections and neurotransmitter type.
+Also prepare fixed attributes as ex/inh and normalized weight.
+
 """
 
 import networkx as nx
 import pandas
+import numpy.random
 
 ##### Load .graphml
 G = nx.read_graphml("elegans.herm_nodesPos.graphml")
 
 
-#####Generate a list of nodes names (two ways)
-
-#ns=list(range(303))
-#for i in range(303):
-#    ns[i]='n'+str(i)
+##### Generate a list of cell names
 
 nsNAME=list(range(302))
 
@@ -48,7 +47,8 @@ for i in range(len(source)):
 
 
 ##### Write lists with connections only beteween neurons
-    #### Removing connections from/to non neuron cells
+####  Thus, removing connections from/to non-neuron cells (sensory/muscles)
+    
 cleandic={}
 cleansource=[]
 cleantarget=[]
@@ -66,15 +66,14 @@ for a in range(len(source)):
                     cleansyn.append(syn[a])
 
 
-##### Lists to .csv
+##### Lists to .csv. First, generate a dictionary with keys:column names, values:column content.
             
 cleandic={'Source':cleansource,'Target':cleantarget,
           'Weight':cleanweight,'Syn':cleansyn}
 
 ##### Write new .csv
 
-connectome=pandas.DataFrame(cleandic, columns=['Source','Target',
-          'Weight','Syn'])
+connectome=pandas.DataFrame(cleandic, columns=['Source','Target','Weight','Syn'])
 connectome.to_csv('/home/jescab01/elegansProject/elegansNet/data/herm_connectome.csv')
 
 
@@ -105,12 +104,37 @@ for i in range(len(n_cleansource)):
     else: G.add_edge(n_cleansource[i],n_cleantarget[i],
                      attr_dict={'Esyn':'True', 'Eweight':cleanweight[i]})
     
+    
+##### Complete edge attributes with Csyn/Esyn=False where necessary.
+    
 for a,b in G.adjacency_iter():
     for c,d in b.items():
         if 'Csyn' not in d:
             d['Csyn']='False'
         elif 'Esyn' not in d:
             d['Esyn']='False'
+            
+            
+#### Normalize synaptic weights
+max_e_weight = 1
+max_c_weight = 1
+
+for n,nbrs in G.adjacency_iter():
+	for nbr,attrs in nbrs.items():
+			if attrs['Esyn'] == 'True':
+				if attrs['Eweight'] > max_e_weight:
+					max_e_weight = attrs['Eweight']
+			if attrs['Csyn'] == 'True':	
+				if attrs['Cweight'] > max_c_weight:
+					max_c_weight = attrs['Cweight']
+
+
+for n,nbrs in G.adjacency_iter():
+	for nbr,attrs in nbrs.items():
+			if attrs['Esyn'] == 'True':
+				attrs['EnormWeight'] = attrs['Eweight'] / max_e_weight
+			if attrs['Csyn'] == 'True':
+				attrs['CnormWeight'] = attrs['Cweight'] / max_c_weight
 
 
 ##### Add characteristic neurotransmitter to nodes as attribute
@@ -124,14 +148,49 @@ nttr = data.Neurotransmitter.tolist()
 
 for a in range(302):
     for b in range(302):
-        if G.node['n'+str(a)]['cell_name'] in neuron[b]:
+        if G.node['n'+str(a)]['cell_name'] == neuron[b]:
             G.node['n'+str(a)]['neurotransmitters']=nttr[b]
+
+
+##### From neurotransmitter attribute, decide if the neuron is excitatory or inhibitory
+            
+NT_types = ['Ach', 'DA', '5HT', 'Glu', 'Ach-5HT', 'Octopamine','Glu-5HT', 'Glu-Tyramine', 'GABA', 'Unknown']
+inh_t=0
+
+
+for i in range(302):
+    if G.node['n'+str(i)]['neurotransmitters'] in NT_types[:8]:
+        G.node['n'+str(i)]['exin']=1
+    elif G.node['n'+str(i)]['neurotransmitters']==NT_types[8]:
+        G.node['n'+str(i)]['exin'] = -1
+    elif G.node['n'+str(i)]['neurotransmitters']==NT_types[9]:
+        G.node['n'+str(i)]['exin'] = 0
+
+### Calculate ratio of inhibitory neurons
+
+for i in range(302):
+   if G.node['n'+str(i)]['exin']==-1:
+        inh_t = inh_t + 1
+inh_ratio = inh_t / 302	
+
+
+## For those cells whose neurotransmitter is unknown, assign ex/inh by ratio
+
+for i in range(302):
+    if G.node['n'+str(i)]['exin']==0:
+        if numpy.random.random()>inh_ratio:
+            G.node['n'+str(i)]['exin'] = 1
+        else:
+            G.node['n'+str(i)]['exin'] = -1
+
+
 
 
 ##### Clear variables
 
-#del a,b,c,i,colnames,data,source,syn,target,weight,nsNAME,connectome,cleansource,cleantarget
-#del n_cleantarget, n_cleansource, cleanweight, cleansyn, neuron, nttr
+del a,b,c,d,i,colnames,data,source,syn,target,weight,nsNAME,connectome,cleansource,cleantarget
+del n_cleantarget, n_cleansource, cleanweight, cleansyn, neuron, nttr, NT_types, inh_t
+del attrs, max_c_weight, max_e_weight, n, nbrs, nbr
             
             
 
